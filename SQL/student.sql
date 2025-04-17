8aab1dd9358c451900d460874668a68a4fc48a1b
balaji koushik s <140054953+balajikoushik2003@users.noreply.github.com>
2025-04-17 14:04:02 +0530

    Add files via upload

---
+++ SQL/student.sql
@@ -0,0 +1,6 @@
+SELECT * FROM student.students;
+INSERT INTO `students` 
+(`studentid`, `studentname`, `studentAge`, `studentdept`, `mobile`, `email`, `date`, `phone1`, `phone2`, `studentimage`) 
+VALUES 
+(87, 'Balaji Koushik S', 22, 'Computer Science', '9876543210', 'balajikoushik07@gmail.com', '2025-03-16', '9876543211', '9876543212', 'balaji.jpg');
+
